b06440eebc26ee7810963bce0d19a48707e27cd9
Eliad Moosavi <iliadm@ca.ibm.com>
2016-09-28 14:20:31 -0400

    Initial Commit

---
+++ app/src/main/java/odbii/starter/automotive/iot/ibm/com/iot4a_odbii/Home.java
@@ -0,0 +1,13 @@
+package odbii.starter.automotive.iot.ibm.com.iot4a_odbii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/test/java/odbii/starter/automotive/iot/ibm/com/iot4a_odbii/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package odbii.starter.automotive.iot.ibm.com.iot4a_odbii;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
